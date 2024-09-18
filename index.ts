3fdccf4820acf812bf7fcdc58e42e694670a3649
nikelborm <29357907+nikelborm@users.noreply.github.com>
2024-09-18 09:23:52 +0300

    save

--- index.ts
+++ index.ts
@@ -1,16 +1,12 @@
 type Merge<T> = { [P in keyof T]: T[P] } & {};
 
 type RemapEnumArrToObject<T extends Array<string> | Array<number>> =
-  T['length'] extends 0
-  ? {}
-  : T['length'] extends 1
-    ? { [k in T[0]] : T[0] }
-    : T extends [
-      infer U extends T[number],
-      ...infer Rest extends Array<string> | Array<number>
-    ]
-      ? { [k in U] : U } & RemapEnumArrToObject<Rest>
-      : never;
+  T extends [
+    infer U extends T[number],
+    ...infer Rest extends Array<string> | Array<number>
+  ]
+    ? { [k in U] : U } & RemapEnumArrToObject<Rest>
+    : {};
 
 const secureObject = (o: Record<string|number|symbol, any>) => {
   Object.seal(o);
@@ -50,8 +46,8 @@ const baseRules = new Set([
   'token [T] is in slot with index [N]',
 
   'there are exactly [N] spatial slots',
-  '[T] OR [U]', // ???
-  '[T] AND [U]', // ???
+  'OR [Rest]', // ???
+  'AND [Rest]', // ???
   'NOT [T]', // ???
   '[T] XOR [U]', // ???
 ]);
@@ -88,21 +84,183 @@ const possibleRules = new Set([
 
 const remapTokens = {
   'token [T] to the left of token [U]': {
-    requires: [
+    searchesFor: [
+      'there is token [T]',
       'there are exactly [N] spatial slots',
     ],
-    results: {
-      type: 'OR',
-      set: [
-        {
-          type: 'AND',
-          set: []
+    results(T: string, U: string, N: number) {
+      const condition = {
+        type: 'OR [Rest]',
+        args: { Rest: [] as any[] }
+      };
+      for (let i = 0; i < N - 1; i++) {
+        for (let j = i + 1; j < N; j++) {
+          condition.args.Rest.push({
+            type: 'AND [Rest]',
+            args: {
+              Rest: [{
+                type: 'token [T] is in slot with index [N]',
+                args: { T: T, N: i,}
+              }, {
+                type: 'token [T] is in slot with index [N]',
+                args: { T: U, N: j,}
+              }]
+            }
+          })
+
         }
-      ] // 
+      }
+      return condition;
     },
-  }
+  },
+  'token of type [T] to the left of token [U]': {
+    searchesFor: [
+      'there is token type [T]',
+      'there is token [U]',
+      'token [T] is of type [U]',
+      'there are exactly [N] spatial slots',
+    ],
+    results(tokensOptionsOfTypeT: string[], U: string) {
+      return {
+        type: 'OR [Rest]',
+        args: {
+          Rest: tokensOptionsOfTypeT.map(T => ({
+            type: 'token [T] to the left of token [U]',
+            args: { T, U }
+          }))
+        }
+      };
+    }
+  },
+  'token [T] to the left of token of type [U]': {
+    searchesFor: [
+      'there is token type [T]',
+      'there is token [U]',
+      'token [T] is of type [U]',
+      'there are exactly [N] spatial slots',
+    ],
+    results(T: string, tokensOptionsOfTypeU: string[]) {
+      return {
+        type: 'OR [Rest]',
+        args: {
+          Rest: tokensOptionsOfTypeU.map(U => ({
+            type: 'token [T] to the left of token [U]',
+            args: { T, U }
+          }))
+        }
+      };
+    }
+  },
+  'token of type [T] to the left of token of type [U]': {
+    searchesFor: [
+      'there is token type [T]',
+      'there is token [U]',
+      'token [T] is of type [U]',
+      'there are exactly [N] spatial slots',
+    ],
+    results(tokensOptionsOfTypeT: string[], tokensOptionsOfTypeU: string[]) {
+      return {
+        type: 'OR [Rest]',
+        args: {
+          Rest: tokensOptionsOfTypeT
+            .flatMap(T => tokensOptionsOfTypeU.map(U => ({ T, U })))
+            .map(args => ({
+              type: 'token [T] to the left of token [U]',
+              args
+            }))
+        }
+      };
+    }
+  },
+
+  'token [T] to the right of token [U]': {
+    results(T: string, U: string) {
+      return {
+        type: 'token [T] to the left of token [U]',
+        args: { T: U, U: T }
+      }
+    },
+  },
+  'token of type [T] to the right of token [U]': {
+    results(T: string, U: string) {
+      return {
+        type: 'token [T] to the left of token of type [U]',
+        args: { T: U, U: T }
+      }
+    },
+  },
+  'token [T] to the right of token of type [U]': {
+    results(T: string, U: string) {
+      return {
+        type: 'token of type [T] to the left of token [U]',
+        args: { T: U, U: T }
+      }
+    },
+  },
+  'token of type [T] to the right of token of type [U]': {
+    results(T: string, U: string) {
+      return {
+        type: 'token of type [T] to the left of token of type [U]',
+        args: { T: U, U: T }
+      }
+    },
+  },
+
+  'token of type [T] must have a neighbour token of type [U]': {
+    results(T: string, U: string) {
+      return {
+        type: 'OR [Rest]',
+        args: {
+          Rest: [
+            'token of type [T] to the left of token of type [U]',
+            'token of type [T] to the right of token of type [U]',
+          ].map(type => ({ type, args: { T, U } })),
+        }
+      };
+    }
+  },
+  'token [T] must have a neighbour token of type [U]': {
+    results(T: string, U: string) {
+      return {
+        type: 'OR [Rest]',
+        args: {
+          Rest: [
+            'token [T] to the left of token of type [U]',
+            'token [T] to the right of token of type [U]',
+          ].map(type => ({ type, args: { T, U } })),
+        }
+      };
+    }
+  },
+  'token of type [T] must have a neighbour token [U]': {
+    results(T: string, U: string) {
+      return {
+        type: 'OR [Rest]',
+        args: {
+          Rest: [
+            'token of type [T] to the left of token [U]',
+            'token of type [T] to the right of token [U]',
+          ].map(type => ({ type, args: { T, U } })),
+        }
+      };
+    }
+  },
+  'token [T] must have a neighbour token [U]': {
+    results(T: string, U: string) {
+      return {
+        type: 'OR [Rest]',
+        args: {
+          Rest: [
+            'token [T] to the left of token [U]',
+            'token [T] to the right of token [U]',
+          ].map(type => ({ type, args: { T, U } })),
+        }
+      };
+    }
+  },
 }
 
+// (A && B && C) || D => (A && B && C) || D
 
 // есть infinite time-spatial slot sequence.
 // на неё можно накладывать правила.
@@ -122,8 +280,6 @@ const remapTokens = {
 
 // some rulesets are AND combination of few rulesets. some rulesets are OR combination of few rulesets.
 // AND rulesets are more restrictive than each of the individual rules. OR rulesets are more allowing than each of the individual rules
-
-// TODO: вспомни про какое-то правило, которое позволяет not or конвертировать в ! AND !
 
 
 // MONARCHS_ENGLISH.MICHAI_FEDOROVICH
--- test-operator.ts
+++ test-operator.ts
@@ -1,7 +1,17 @@
 import { equal } from 'assert';
+import type { List } from "ts-toolbelt"
 import test from 'node:test';
 
-function testComparator(
+function logObjectNicely(item: any): void {
+  console.dir(item, {
+    colors: true,
+    compact: false,
+    depth: null,
+  });
+}
+
+
+function testXorComparator(
   name: string,
   comparator: (a: boolean, b: boolean) => boolean,
 ) {
@@ -13,15 +23,91 @@ function testComparator(
   })
 }
 
-testComparator('original xor', (a, b) => a !== b)
+testXorComparator('original xor', (a, b) => a !== b)
 
-testComparator('1st custom xor using: !, &&    ', (a, b) => !(a && b) && !(!a && !b)) // 7 ops
-testComparator('2nd custom xor using: !, &&    ', (a, b) => !( !(a && !b) && !(!a && b) )) // 8 ops
+testXorComparator('1st custom xor using: !, &&    ', (a, b) => !(a && b) && !(!a && !b)) // 7 ops
+testXorComparator('2nd custom xor using: !, &&    ', (a, b) => !( !(a && !b) && !(!a && b) )) // 8 ops
 
-testComparator('1st custom xor using: !, &&, ||', (a, b) => !(a && b) && (a || b)) // 4 ops
-testComparator('2nd custom xor using: !, &&, ||', (a, b) => (a && !b) || (!a && b)) // 5 ops
-testComparator('3rd custom xor using: !, &&, ||', (a, b) => !( (a && b) || !(a || b) )) // 5 ops
-testComparator('4th custom xor using: !, &&, ||', (a, b) => (!a || !b) && (a || b)) // 5 ops
-testComparator('5th custom xor using: !, &&, ||', (a, b) => !( (a && b) || (!a && !b) )) // 6 ops
+testXorComparator('1st custom xor using: !, &&, ||', (a, b) => !(a && b) && (a || b)) // 4 ops
+testXorComparator('2nd custom xor using: !, &&, ||', (a, b) => (a && !b) || (!a && b)) // 5 ops
+testXorComparator('3rd custom xor using: !, &&, ||', (a, b) => !( (a && b) || !(a || b) )) // 5 ops
+testXorComparator('4th custom xor using: !, &&, ||', (a, b) => (!a || !b) && (a || b)) // 5 ops
+testXorComparator('5th custom xor using: !, &&, ||', (a, b) => !( (a && b) || (!a && !b) )) // 6 ops
 
-testComparator('1st custom xor using: !, ||', (a, b) => !( !(!a || !b) || !(a || b) )) // 8 ops
+testXorComparator('1st custom xor using: !, ||', (a, b) => !( !(!a || !b) || !(a || b) )) // 8 ops
+
+
+test('testMulti', () => {
+  const k = 2;
+  const operation = (a: number, b: number) => a | b;
+
+  for (let i = 0; i < 2 ** k; i++) {
+    let temp = i & 1;
+    for (let j = 1; j < k; j++) {
+      temp = operation(temp, ~~!!(i & (2 ** j)))
+    }
+    // console.log(i.toString(2).padStart(k, '0'), temp)
+  }
+});
+
+
+function testFunctionOnAllParameters<
+  const Length extends number
+>(
+  functionParameters: Length,
+  debug: boolean,
+  func1: (...args: List.Repeat<boolean, NoInfer<Length>>) => boolean,
+  func2: (...args: List.Repeat<boolean, NoInfer<Length>>) => boolean,
+) {
+  test(`testFunctionOnAllParameters: ${functionParameters}, ${func1}, ${func2}`, () => {
+    for (let i = 0; i < 2 ** functionParameters; i++) {
+      const args = [] as boolean[];
+      for (let j = 0; j < functionParameters; j++) {
+        args.push(!!(i & (2 ** j)))
+      }
+      const res1 = func1.apply(null, args)
+      const res2 = func2.apply(null, args)
+      if (debug) console.log(
+        `${args.map(e => ~~e).join(', ')} => ${~~res1}, ${~~res2}`
+      );
+      equal(
+        res1,
+        res2,
+        `func1, func2 => ${res1}, ${res2}; args=[${args.join(', ')}]`
+      )
+    }
+  })
+}
+
+testFunctionOnAllParameters(
+  4,
+  false,
+  (A, B, C, D) => (A || B || C) && D,
+  (A, B, C, D) => (A && D)
+               || (B && D)
+               || (C && D),
+);
+
+
+testFunctionOnAllParameters(
+  6,
+  false,
+  (A, B, C, D, E, F) => (A || B || C) && (D || E || F),
+  (A, B, C, D, E, F) => (A && D) || (A && E) || (A && F) ||
+                        (B && D) || (B && E) || (B && F) ||
+                        (C && D) || (C && E) || (C && F)
+);
+
+testFunctionOnAllParameters(
+  6,
+  false,
+  (A, B, C, D, E, F) => (A && B && C) || (D && E && F),
+  (A, B, C, D, E, F) => !(!(A && B && C) && !(D && E && F))
+);
+
+testFunctionOnAllParameters(
+  3,
+  false,
+  (A, B, C) => !(A && B && C),
+  (A, B, C) => !A || !B || !C,
+);
